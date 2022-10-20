b4d6b6113dc5dc1638abc15a57e4f8ad21159cab
Jamie Mair <JamieMair@users.noreply.github.com>
2022-10-20 14:31:08 +0000

    Started on defining primatives for the module

--- src/SimpleNNs.jl
+++ src/SimpleNNs.jl
@@ -1,5 +1,36 @@
 module SimpleNNs
 
-# Write your package code here.
+# Abstract types
+abstract type AbstractLayer end
+
+# Layer types
+struct Static{N} <: AbstractLayer
+    inputs::NTuple{N, Int}
+end
+Base.@kwdef struct Dense{T<:Function} <: AbstractLayer
+    outputs::Int
+    inputs::Union{Symbol, Int} = :infer
+    use_bias::Bool = true
+    activation_fn::T = identity
+end
+
+# Model
+struct Model{T,Q}
+    parameters::T
+    layers::Q
+end
+
+# Activation functions
+sigmoid(x) = inv(one(typeof(x) + exp(-x)))
+relu(x) = ifelse(x>=zero(typeof(x)), x, zero(typeof(x)))
+
+function chain(layers...)
+    first_layer = first(layers)
+    if !(first_layer isa Static)
+        @error "The first layer should always be a static layer, specifying the input size."
+    end
+    input_size = first_layer.inputs
+    # todo - continue this work!
+end
 
 end
