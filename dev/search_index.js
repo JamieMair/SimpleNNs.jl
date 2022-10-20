9f0b2c3b80b3d0472530c58c27948be3d7b16b31
Documenter.jl <documenter@juliadocs.github.io>
2022-10-20 13:28:39 +0000

    build based on e775829

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SimpleNNs","category":"page"},{"location":"#SimpleNNs","page":"Home","title":"SimpleNNs","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SimpleNNs.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SimpleNNs]","category":"page"}]
+}
