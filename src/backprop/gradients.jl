e2cf0cf8ab75fff4d18bd3e87d2c826cacf0048f
Jamie Mair <JamieMair@users.noreply.github.com>
2023-06-04 07:26:30 +0000

    Corrected the gradient calculation to be done w.r.t the outputs, not the activations themselves

--- src/backprop/gradients.jl
+++ src/backprop/gradients.jl
@@ -1,29 +1,30 @@
 # Backpropagation algorithm for calculating gradients
-function sech2(x)
-    ex = exp(x)
-    sum_exs = (inv(ex)+ex)
-    return convert(typeof(x), 4) / (sum_exs*sum_exs)
+function tanh_derivative(y) # w.r.t the output
+    return (one(typeof(y))-y*y)
 end
-relu_derivative(x) = ifelse(x>zero(x), one(x), zero(x))
-function sigmoid_derivative(x)
-    ex = exp(x)
-    inv_ex_plusone = inv(ex + one(x))
-    return ex * inv_ex_plusone * inv_ex_plusone 
+relu_derivative(x) = ifelse(x>zero(x), one(x), zero(x)) # w.r.t the output
+function sigmoid_derivative(y) # w.r.t the output y
+    return (one(typeof(y))-y)*y
 end
 
+"""
+    activation_gradient_fn
+
+The gradient function of a layer w.r.t to its OUTPUT (not the activation).
+"""
 activation_gradient_fn(::Dense{DT, K, T}) where {DT, K, T} = activation_gradient_fn(Val(T))
 activation_gradient_fn(c) = activation_gradient_fn(Val(typeof(c.activation_fn)))
 function activation_gradient_fn(::Val{T}) where {T}
     if T === typeof(identity)
         return one
     elseif T === typeof(tanh)
-        return sech2
+        return tanh_derivative
     elseif T === typeof(relu)
         return relu_derivative
     elseif T === typeof(sigmoid)
         return sigmoid_derivative
     elseif T === typeof(tanh_fast)
-        return sech2
+        return tanh_derivative
     else
         return unimplemented()
     end
