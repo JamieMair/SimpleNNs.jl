6028e634091c066684cfce17f71751b2a3c3d4fb
Jamie Mair <JamieMair@users.noreply.github.com>
2023-06-04 07:42:40 +0000

    Corrected the comments to better reflect the changes

--- src/backprop/gradients.jl
+++ src/backprop/gradients.jl
@@ -1,16 +1,18 @@
-# Backpropagation algorithm for calculating gradients
-function tanh_derivative(y) # w.r.t the output
+function tanh_derivative(y)
     return (one(typeof(y))-y*y)
 end
-relu_derivative(x) = ifelse(x>zero(x), one(x), zero(x)) # w.r.t the output
-function sigmoid_derivative(y) # w.r.t the output y
+relu_derivative(y) = ifelse(y>zero(y), one(y), zero(y))
+function sigmoid_derivative(y)
     return (one(typeof(y))-y)*y
 end
 
 """
-    activation_gradient_fn
+Dertivatives are used to backpropagate the gradients of the layer outputs back to
+the activations of that layer. To save space, these are calculated exclusively
+using the outputs of the layer. Instead of functions written as dy/dx=f(x), we 
+instead write dy/dx = g(y). This can be done for the 3 major functions.
 
-The gradient function of a layer w.r.t to its OUTPUT (not the activation).
+Whenever `y` is used below, assume this is a function of the output, not the input.
 """
 activation_gradient_fn(::Dense{DT, K, T}) where {DT, K, T} = activation_gradient_fn(Val(T))
 activation_gradient_fn(c) = activation_gradient_fn(Val(typeof(c.activation_fn)))
